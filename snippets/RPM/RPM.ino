e6c2e9654ef7bfcdd37cb41ac7c2190cdc11f558
Miltiadis Stouras <m.stouras@gmail.com>
2019-05-18 21:25:44 +0300

    Move RPM test file to snippets/RPM

---
+++ snippets/RPM/RPM.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+
+double getRPM() {
+  byte data[3];
+  Wire.requestFrom(RPM_ADDR, 3);
+  for (byte i=0; i<NUM_BYTES; i++)
+    data[i] = Wire.read();
+
+  uint32_t d = ((uint32_t)data[0] << 16) + ((uint32_t)data[1] << 8) + ((uint32_t)data[2]);
+  return (double)(60.0 / (d * 0.000004));
+}
+
+uint32_t timer;
+double rpm;
+
+void setup(){
+  Serial.begin(9600);
+  timer = millis();
+}
+
+void loop(){
+  if(millis() - timer >= 500){
+    rpm = getRPM();
+    Serial.println("RPM: "+String(rpm));    
+  }
+}
