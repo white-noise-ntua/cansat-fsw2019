306f07b7f2c20b21ccf43b40a4a3180b3c71d790
Miltiadis Stouras <m.stouras@gmail.com>
2019-05-18 21:00:31 +0300

    Add some initialization parameters in BMP280 setup

--- snippets/BMP280/BMP280.ino
+++ snippets/BMP280/BMP280.ino
@@ -13,6 +13,12 @@ void setup(){
       Serial.println("Could not find a valid BMP280 sensor, check wiring!");
       while (1);
   }
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
   timer = millis();
 }
 
