a142fcbad6d726c73030c3864991c533fb4aa991
Miltiadis Stouras <m.stouras@gmail.com>
2019-05-18 22:10:58 +0300

    Change getRPM() function to be consistent with the others

--- flightSoftware/flightSoftware.ino
+++ flightSoftware/flightSoftware.ino
@@ -50,7 +50,7 @@ float latitude;
 float longitude;
 float gpsAltitude;
 int gpsSats;
-float spinRate;
+double spinRate;
 
 float pitch,roll; // Change them with the code from control
 
@@ -313,14 +313,14 @@ void readGyro(){
   euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
 }
 
-double getRPM() {
+void readRPM() {
   byte data[3];
   Wire.requestFrom(RPM_ADDR, 3);
   for (byte i=0; i<NUM_BYTES; i++)
     data[i] = Wire.read();
 
   uint32_t d = ((uint32_t)data[0] << 16) + ((uint32_t)data[1] << 8) + ((uint32_t)data[2]);
-  return (double)(60.0 / (d * 0.000004));
+  spinRate =  (double)(60.0 / (d * 0.000004));
 }
 
 // === Functions for RTC ===
