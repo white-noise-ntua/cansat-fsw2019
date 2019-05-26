3f7a115468d9bf1f766ad3611e23bdbf74cbdede
Miltiadis Stouras <m.stouras@gmail.com>
2019-05-26 14:04:39 +0300

    In BMP snippet, add communication through Xbee on Serial2

--- snippets/BMP280/BMP280.ino
+++ snippets/BMP280/BMP280.ino
@@ -6,10 +6,16 @@ Adafruit_BMP280 bmp; // I2C
 uint32_t timer;
 float temperature;
 float pressure;
+float altitude;
+float altitude0;
 
 void setup(){
   Serial.begin(9600); // start serial monitor
 
+  Serial2.begin(115200);
+
+  Serial2.println("HELLO BITCHES");
+  
   if (!bmp.begin()) {
       Serial.println("Could not find a valid BMP280 sensor, check wiring!");
       while (1);
@@ -21,6 +27,7 @@ void setup(){
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
   timer = millis();
+  altitude0 = bmp.readAltitude(1019.666);  
 }
 
 void loop(){
@@ -28,8 +35,11 @@ void loop(){
     timer = millis();
     temperature = bmp.readTemperature();
     pressure = bmp.readPressure();
-    String packet = "Temperature: " + String(temperature) + ",  Pressure: " + String(pressure);
+    altitude = bmp.readAltitude(1019.66)-altitude0;
+    String packet = "Temperature: " + String(temperature) + ",  Pressure: " + String(pressure) + ", " + String(altitude);
     Serial.println(packet);
+
+    Serial2.println(packet);
 
   }
 }
