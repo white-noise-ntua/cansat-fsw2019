4b0cb60744aa87881c06f70177e3465c846401bf
Miltiadis Stouras <m.stouras@gmail.com>
2019-06-04 21:02:48 +0300

    Add new RTC snippet

--- snippets/RTC/RTC.ino
+++ snippets/RTC/RTC.ino
@@ -1,49 +1,111 @@
 #include <Wire.h>
-#include <TimeLib.h>
-#include <DS1307RTC.h>
+#include <RTCx.h>
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial) ; // wait for serial
-  delay(200);
-  Serial.println("DS1307RTC Read Test");
-  Serial.println("-------------------");
+void setup(void)
+{
+	Serial.begin(9600);
+	Wire.begin();
+	Serial.println();
+	Serial.println("Autoprobing for a RTC...");
+	if (rtc.autoprobe()) {
+		// Found something, hopefully a clock.
+		Serial.print("Autoprobe found ");
+		Serial.print(rtc.getDeviceName());
+		Serial.print(" at 0x");
+		Serial.println(rtc.getAddress(), HEX);
+	}
+	else {
+		// Nothing found at any of the addresses listed.
+		Serial.println("No RTCx found, cannot continue");
+		while (1)
+			;
+	}
+	// Ensure the oscillator is running.
+	//rtc.startClock();
 }
 
-void loop() {
-  tmElements_t tm;
+void loop(void)
+{
+	struct RTCx::tm tm;
+	if (millis() - last > 500) {
+		last = millis();
+		rtc.readClock(tm);
 
-  if (RTC.read(tm)) {
-    Serial.print("Ok, Time = ");
-    print2digits(tm.Hour);
-    Serial.write(':');
-    print2digits(tm.Minute);
-    Serial.write(':');
-    print2digits(tm.Second);
-    Serial.print(", Date (D/M/Y) = ");
-    Serial.print(tm.Day);
-    Serial.write('/');
-    Serial.print(tm.Month);
-    Serial.write('/');
-    Serial.print(tmYearToCalendar(tm.Year));
-    Serial.println();
-  } else {
-    if (RTC.chipPresent()) {
-      Serial.println("The DS1307 is stopped.  Please run the SetTime");
-      Serial.println("example to initialize the time and begin running.");
-      Serial.println();
-    } else {
-      Serial.println("DS1307 read error!  Please check the circuitry.");
-      Serial.println();
-    }
-    delay(9000);
-  }
-  delay(1000);
-}
+//		RTCx::printIsotime(Serial, tm).println()/;
+		RTCx::time_t t = RTCx::mktime(&tm);
 
-void print2digits(int number) {
-  if (number >= 0 && number < 10) {
-    Serial.write('0');
-  }
-  Serial.print(number);
+		Serial.print("unixtime = ");
+		Serial.println(t);
+		Serial.println("-----");
+	}
+
+//	while (Serial.available()) {
+//		char c = Serial.read();
+//		if ((c == '\r' || c == '\n' || c == '\0')) {
+//			if (bufPos <= bufLen && buffer[0] == 'C') {
+//				// Check time error
+//				buffer[bufPos] = '\0';
+//				RTCx::time_t pcTime = atol(&(buffer[1]));
+//				rtc.readClock(&tm);
+//				RTCx::time_t mcuTime = RTCx::mktime(&tm);
+//				Serial.print("MCU clock error: ");
+//				Serial.print(mcuTime - pcTime);
+//				Serial.println(" s");
+//				Serial.println("~~~~~");
+//			}
+//			if (bufPos <= bufLen && buffer[0] == 'T') {
+//				// Set time
+//				buffer[bufPos] = '\0';
+//				RTCx::time_t t = atol(&(buffer[1]));
+//				RTCx::gmtime_r(&t, &tm);
+//				rtc.setClock(&tm);
+//				Serial.println("Clock set");
+//				Serial.println(&(buffer[0]));
+//				RTCx::printIsotime(Serial, tm);
+//				Serial.println("~~~~~");
+//			}
+//			if (bufPos <= bufLen && buffer[0] == 'X') {
+//				// Set calibration value
+//				buffer[bufPos] = '\0';
+//				if (rtc.getDevice() == RTCx::MCP7941x) {
+//					int8_t oldCal = rtc.getCalibration();
+//					char *endptr;
+//					long cal = strtol(&(buffer[1]), &endptr, 0);
+//					if (cal >= -127 && cal <= 127 && endptr == &buffer[bufPos]) {
+//						Serial.print("Previous calibration: ");
+//						Serial.println(oldCal, DEC);
+//						Serial.print("Calibration: ");
+//						Serial.println(cal, DEC);
+//						rtc.setCalibration(cal);
+//					}
+//					else
+//						Serial.println("Bad value for calibration");
+//				}
+//				else {
+//					Serial.println("Cannot set calibration: not a MCP7941x");
+//				}
+//			}
+//			if (bufPos <= bufLen && buffer[0] == 'M') {
+//				// Set SQW mode
+//				buffer[bufPos] = '\0';
+//				char *endptr;
+//				long mode = strtol(&(buffer[1]), &endptr, 0);
+//				if (mode >= RTCx::freq1Hz && mode <= RTCx::freqCalibration
+//					&& endptr == &buffer[bufPos]) {
+//					if (rtc.setSQW((RTCx::freq_t)mode)) {
+//						Serial.print("SQW: ");
+//						Serial.println(mode, DEC);
+//					}
+//					else
+//						Serial.println("Could not set SQW");
+//				}
+//				else
+//					Serial.println("Bad value for SQW");
+//			}
+//			bufPos = 0;
+//		}
+//		else if (bufPos < bufLen)
+//			// Store character
+//			buffer[bufPos++] = c;
+//	}
 }
